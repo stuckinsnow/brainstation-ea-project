3aaf7ad8c8342a4fb82c333f1cd93a4d39976b9b
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-06-15 21:12:59 -0700

    add header.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.scss';
+
+
+function Header() {
+  return (
+    <header className="App-header">
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste numquam impedit doloribus illum qui dicta quis non sint, voluptas quam tempore quaerat? Ad, perspiciatis officiis recusandae quia quod aliquid quae.</div>
+    </header>
+  );
+}
+
+export default Header; 
