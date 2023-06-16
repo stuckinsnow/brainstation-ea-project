38ec916ca65c2453afe642e269e49491b366cf55
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-16 11:24:19 -0700

    set up layout for brotbrawl.

--- src/pages/BotbrawlPage/BotbrawlPage.jsx
+++ src/pages/BotbrawlPage/BotbrawlPage.jsx
@@ -1,5 +1,7 @@
 import './BotbrawlPage.scss';
 
+
+
 function BotbrawlPage() {
     return (
         <div>
@@ -9,20 +11,41 @@ function BotbrawlPage() {
             </header>
 
             <main>
+                <section className='maincontent'>
+                    <h1 className='maincontent__title'>Bot Brawl Vocal Training Academy</h1>
 
-                <section>
-                    <h1>Bot Brawl Vocal Training Academy</h1>
-
-                    <div>
-                        Transcript from last game
-                    </div>
-                    <div id="videoPlayer">
-
+                    <div className='maincontent__card maincontent__left'>
+                        <div className="maincontent__left--videoplayer">
+                            VIDEO COMPONENT GOES HERE
+                        </div>
                     </div>
 
-                    <div className="chatGPT">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat maxime nam, nemo ab odit dolores voluptas dolorum provident illo doloribus quidem, officiis error deleniti soluta. Ea sapiente qui quidem minima!
+                    <div className='maincontent__card maincontent__right'>
+                        <div>
+                            Transcript from last game
+                        </div>
+
+                        <div className="chatGPT">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat maxime nam, nemo ab odit dolores voluptas dolorum provident illo doloribus quidem, officiis error deleniti soluta. Ea sapiente qui quidem minima!
+                        </div>
+
+
+                        <div className='maincontent__right--icons'>
+                            <div className=''>
+                                Up
+                            </div>
+
+                            <div className=''>
+                                Down
+                            </div>
+                        </div>
+
+
                     </div>
+
+
+
+
 
                 </section>
 
