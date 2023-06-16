d9572a49bbe6daf1877644abfe90ed1d91cf3c34
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-06-16 14:12:20 -0700

    fix header.

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -3,48 +3,54 @@ import "./HeaderNav.scss";
 import logo from "../../assets/logo/logo.png";
 import navIcon from "../../assets/icons/uiw_down.png";
 
+
+function handleClick() {
+  window.location.href = "/";
+}
+
+
 function HeaderNav() {
   return (
     <header>
       <div className="header">
         <div className="header__icons"></div>
         <div className="header__links">
-          <div className="header__logo">
-            <img src={logo} alt="Apex Legends Logo" />
-          </div>
+        <div className="header__logo" onClick={handleClick}>
+      <img src={logo} alt="Apex Legends Logo" />
+    </div>
           <div className="header__nav">
             <nav className="header__nav">
               <div className="header__nav--drop-down">
-                <Link to="/">About</Link>
+                <Link to="/botbrawl">About</Link>
                 <img src={navIcon} alt="" />
               </div>
 
               <div className="header__nav--drop-down">
-                <Link to="/about">Moes</Link>
+                <Link to="/botbrawl">Moes</Link>
                 <img src={navIcon} alt="" />
               </div>
 
-              <Link to="/users">Season</Link>
-              <Link to="/contact">Battlepass</Link>
+              <Link to="/botbrawl">Season</Link>
+              <Link to="/botbrawl">Battlepass</Link>
 
               <div className="header__nav--drop-down">
-                <Link to="/about">News & Media</Link>
-                <img src={navIcon} alt="" />
-              </div>
-
-              <div className="header__nav--drop-down">
-                <Link to="/users">Community</Link>
+                <Link to="/botbrawl">News & Media</Link>
                 <img src={navIcon} alt="" />
               </div>
 
               <div className="header__nav--drop-down">
-                <Link to="/contact">Complete</Link>
+                <Link to="/botbrawl">Community</Link>
+                <img src={navIcon} alt="" />
+              </div>
+
+              <div className="header__nav--drop-down">
+                <Link to="/botbrawl">Complete</Link>
                 <img src={navIcon} alt="" />
               </div>
             </nav>
           </div>
           <div className="header__btn">
-            <button>Train your bot</button>
+          <button onClick={() => { window.location.href = '/botbrawl'; }}>Train your bot</button>
           </div>
         </div>
       </div>
